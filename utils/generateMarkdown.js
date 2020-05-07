3cd212186dc18234633d06ff9a7122d2accbd50f
DJS <davidstahl@Davids-MacBook-Pro.local>
2020-05-07 15:32:24 -0700

    initial structure of readme.md page complete

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -2,6 +2,36 @@
 function generateMarkdown(data) {
   return `# ${data.title}
 
+          ## Description 
+          ${data.description}
+
+          ## Table of Contents (links need to be live)
+            * [Installation](#installation)
+            * [Usage](#usage)
+            * [Credits](#credits)
+            * [License](#license)
+            * [Badges](#badges)
+            * [Contributing](#contributing)
+            * [Tests](#tests) 
+
+
+          ## Installation Instructions <a name="installation"></a> 
+          ${data.installation}
+
+          ## Usage Instructions <a name="usage"></a>
+          ${data.usage}
+
+          ## Credits <a name="credits"></a>
+
+          ## License <a name="license"></a>
+
+          ## Badges <a name="badges"></a>
+
+          ## Contributing <a name="contributing"></a>
+
+          ## Tests <a name="tests"></a>
+
+
 `;
 }
 
